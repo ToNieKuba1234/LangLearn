d6cec747f68155743193e67266b00a2e74d63a5c
Mariusz Malinowski <mariusz.malinowski@akra.de>
2026-03-26 13:49:52 +0100

    tailwindcss init

---
+++ src/main/resources/static/tailwind.config.mjs
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    "./src/main/resources/templates/**/*.{html,thymeleaf}",
+    "./src/main/resources/static/src/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {}
+  }
+}
+
