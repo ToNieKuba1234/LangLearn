9664242a726ee1a4abcc608a7053b49d0a2b905a
ToNieKuba <jakub.kasprzak009@gmail.com>
2026-03-26 15:01:38 +0100

    added trophies to users db

--- src/main/java/com/jakkas/langlearn/model/Users.java
+++ src/main/java/com/jakkas/langlearn/model/Users.java
@@ -5,24 +5,47 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
+import jakarta.persistence.Table;
 
 @Entity
-@Data //auto getters/setters
+@Table(name = "users")
 public class Users {
     @Id 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = false)
+    @Column(unique = true, nullable = false, name = "username")
     private String username;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "password")
     private String password;
+    
+    @Column(name = "trophies", nullable = false)
+    private Integer trophies = 0;
 
 
-    public String getUsername() {return username; }
-    public String getPassword() {return password; }
-    public void setUsername(String username) {this.username = username; }
-    public void setPassword(String password) {this.password = password; }
+    //username 
+    public String getUsername() {
+        return username; 
+    }
+    public void setUsername(String username) {
+        this.username = username; 
+    }
+
+    
+    //password
+    public String getPassword() {
+        return password;   
+    }
+    public void setPassword(String password) {
+        this.password = password; 
+    }
+
+    //trophies
+    public Integer getTrophies() {
+        return trophies;
+    }
+    public void setTrophies(int trophies) {
+        this.trophies = trophies;
+    }
 }
