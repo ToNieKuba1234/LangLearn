af4486d6947fc381384016376a7017aa249e13d4
ToNieKuba <jakub.kasprzak009@gmail.com>
2026-04-14 14:41:13 +0200

    flyway k8s config

--- k8s-destroy.sh
+++ k8s-destroy.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
+kubectl delete -f k8s-setup.yaml --ignore-not-found=true
+
+kubectl delete deployments --all
+kubectl delete services --all
+
+kubectl delete pvc --all
+
 minikube stop
-minikube delete
--- langlearn-backend/src/main/java/com/jakkas/langlearn/LangLearnBackendApplication.java
+++ langlearn-backend/src/main/java/com/jakkas/langlearn/LangLearnBackendApplication.java
@@ -1,28 +1,12 @@
 package com.jakkas.langlearn;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.jakkas.langlearn.repository.UsersRepository;
-import com.jakkas.langlearn.service.UsersService;
 
 @SpringBootApplication
 public class LangLearnBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LangLearnBackendApplication.class, args);
-    }
-
-    // Tworzenie testowego konta
-    @Bean
-    public CommandLineRunner initData(UsersService usersService, UsersRepository repository) {
-        return args -> {
-            if (repository.findByUsername("jakk").isEmpty()) {
-                usersService.registerUser("jakk", "kuba123");
-                System.out.println("Test account created successfully: jakk / kuba123");
-            }
-        };
     }
 }
