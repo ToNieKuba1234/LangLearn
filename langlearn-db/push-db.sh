fd5ed5ad2324eedc852209b83f12581b694fb0d6
ToNieKuba <jakub.kasprzak009@gmail.com>
2026-04-14 10:35:32 +0200

    flyway local config

--- langlearn-db/push-db.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DB_URL=$(minikube service langlearn-db-service --url | sed 's/http/jdbc:postgresql/')
-
-flyway migrate -url=$DB_URL -user=user -password=pass -locations=filesystem:src/main/resources/db/migration
---
+++ langlearn-db/src/main/resources/db/migration/V1776076968__initial_data.sql
@@ -0,0 +1,55 @@
+-- noun
+INSERT INTO words (german_word, polish_word) VALUES ('Das Haus', 'Dom');
+INSERT INTO words (german_word, polish_word) VALUES ('Der Hund', 'Pies');
+INSERT INTO words (german_word, polish_word) VALUES ('Die Katze', 'Kot');
+INSERT INTO words (german_word, polish_word) VALUES ('Der Apfel', 'Jabłko');
+INSERT INTO words (german_word, polish_word) VALUES ('Das Buch', 'Książka');
+INSERT INTO words (german_word, polish_word) VALUES ('Die Schule', 'Szkoła');
+INSERT INTO words (german_word, polish_word) VALUES ('Der Tisch', 'Stół');
+INSERT INTO words (german_word, polish_word) VALUES ('Das Fenster', 'Okno');
+INSERT INTO words (german_word, polish_word) VALUES ('Die Stadt', 'Miasto');
+INSERT INTO words (german_word, polish_word) VALUES ('Der Schlüssel', 'Klucz');
+INSERT INTO words (german_word, polish_word) VALUES ('Das Wasser', 'Woda');
+INSERT INTO words (german_word, polish_word) VALUES ('Der Kaffee', 'Kawa');
+INSERT INTO words (german_word, polish_word) VALUES ('Die Brot', 'Chleb');
+INSERT INTO words (german_word, polish_word) VALUES ('Das Handy', 'Telefon komórkowy');
+INSERT INTO words (german_word, polish_word) VALUES ('Der Computer', 'Komputer');
+
+-- verbs
+INSERT INTO words (german_word, polish_word) VALUES ('Essen', 'Jeść');
+INSERT INTO words (german_word, polish_word) VALUES ('Trinken', 'Pić');
+INSERT INTO words (german_word, polish_word) VALUES ('Schlafen', 'Spać');
+INSERT INTO words (german_word, polish_word) VALUES ('Laufen', 'Biegać');
+INSERT INTO words (german_word, polish_word) VALUES ('Sprechen', 'Mówić');
+INSERT INTO words (german_word, polish_word) VALUES ('Lesen', 'Czytać');
+INSERT INTO words (german_word, polish_word) VALUES ('Schreiben', 'Pisać');
+INSERT INTO words (german_word, polish_word) VALUES ('Gehen', 'Iść');
+INSERT INTO words (german_word, polish_word) VALUES ('Sehen', 'Widzieć');
+INSERT INTO words (german_word, polish_word) VALUES ('Hören', 'Słuchać');
+INSERT INTO words (german_word, polish_word) VALUES ('Machen', 'Robić');
+INSERT INTO words (german_word, polish_word) VALUES ('Lernen', 'Uczyć się');
+INSERT INTO words (german_word, polish_word) VALUES ('Arbeiten', 'Pracować');
+INSERT INTO words (german_word, polish_word) VALUES ('Wohnen', 'Mieszkać');
+INSERT INTO words (german_word, polish_word) VALUES ('Kaufen', 'Kupować');
+
+-- adjective
+INSERT INTO words (german_word, polish_word) VALUES ('Groß', 'Duży');
+INSERT INTO words (german_word, polish_word) VALUES ('Klein', 'Mały');
+INSERT INTO words (german_word, polish_word) VALUES ('Gut', 'Dobry');
+INSERT INTO words (german_word, polish_word) VALUES ('Schlecht', 'Zły');
+INSERT INTO words (german_word, polish_word) VALUES ('Schön', 'Piękny');
+INSERT INTO words (german_word, polish_word) VALUES ('Schnell', 'Szybki');
+INSERT INTO words (german_word, polish_word) VALUES ('Langsam', 'Powolny');
+INSERT INTO words (german_word, polish_word) VALUES ('Alt', 'Stary');
+INSERT INTO words (german_word, polish_word) VALUES ('Jung', 'Młody');
+INSERT INTO words (german_word, polish_word) VALUES ('Kalt', 'Zimny');
+INSERT INTO words (german_word, polish_word) VALUES ('Warm', 'Ciepły');
+INSERT INTO words (german_word, polish_word) VALUES ('Glücklich', 'Szczęśliwy');
+INSERT INTO words (german_word, polish_word) VALUES ('Traurig', 'Smutny');
+INSERT INTO words (german_word, polish_word) VALUES ('Teuer', 'Drogi');
+INSERT INTO words (german_word, polish_word) VALUES ('Billig', 'Tani');
+INSERT INTO words (german_word, polish_word) VALUES ('Einfach', 'Prosty');
+INSERT INTO words (german_word, polish_word) VALUES ('Schwierig', 'Trudny');
+INSERT INTO words (german_word, polish_word) VALUES ('Immer', 'Zawsze');
+INSERT INTO words (german_word, polish_word) VALUES ('Nie', 'Nigdy');
+INSERT INTO words (german_word, polish_word) VALUES ('Heute', 'Dzisiaj');
